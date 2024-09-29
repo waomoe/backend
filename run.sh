8f5bb259b744b00ec56cd9f95e12c72884c8f583
nichind <nichinddev@gmail.com>
2024-09-29 16:44:34 +0330

    add .sh for prod

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 # !/bin/bash
 
 host=localhost
-port=7741
+port=8741
 source .venv/bin/activate
 python_executable=$(which python3.11)
 echo "Starting wao.moe backend on $host:$port with $python_executable" 
 $python_executable -m venv .venv
 $python_executable -m pip install --upgrade pip
 $python_executable -m pip install -r requirements.txt
-$python_executable -m uvicorn app:app --host $host --port $port --reload
+$python_executable -m uvicorn app:app --host $host --port $port
